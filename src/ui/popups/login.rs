87fcd243927f23c739d62a79b4b1f5f543a03afb
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-02-22 12:48:33 +0300

    Improve login popup

--- src/ui/popups/login.rs
+++ src/ui/popups/login.rs
@@ -1,6 +1,6 @@
-use egui::{Align, Layout};
+use egui::{Align, Layout, RichText, Color32};
 
-use crate::ui::widget_signal::{AppSignal, StateSignal};
+use crate::{ui::widget_signal::{AppSignal, StateSignal}, utils::is_valid_email};
 
 use super::popup_builder::PopupBuilder;
 
@@ -29,31 +29,53 @@ impl<'a> PopupBuilder<'a> for Login {
         _ctx: &'a egui::Context,
     ) -> Box<dyn FnOnce(&mut egui::Ui) -> egui::Response + 'a> {
         self.signals.clear();
-        Box::new(|ui: &mut egui::Ui| {
-            ui.with_layout(Layout::top_down(Align::LEFT), |ui| {
-                egui::Grid::new("login")
-                    .max_col_width(ui.available_width())
-                    .show(ui, |ui| {
-                        ui.label("Email: ");
-                        ui.text_edit_singleline(&mut self.email)
-                            .on_hover_text("Email");
-                        ui.end_row();
+        Box::new(|ui| {
+            let email_error: Option<String> = {
+                (&self.email != "admin" && !is_valid_email(&self.email))
+                    .then_some("Email is not valid".to_owned())
+            };
+            let password_error: Option<String> = {
+                /*(!is_valid_password(&self.password))
+                    .then_some("Invalid password".to_owned())
+                    .or((!is_strong_enough_password(&self.password))
+                        .then_some("Password is not strong enough".to_string()))*/
+                None
+            };
 
-                        ui.label("Password: ");
-                        ui.text_edit_singleline(&mut self.password)
-                            .on_hover_text("Password");
-                        ui.end_row();
-                    });
-                ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
-                    // RTL
-                    if ui.button("Login").clicked() {
-                        self.signals.push(
-                            StateSignal::Login(self.email.clone(), self.password.clone()).into(),
+            let error = email_error.as_ref()
+                .or(password_error.as_ref());
+
+            let show_input_field = |ui: &mut egui::Ui, value: &mut String, hint: &str| {
+                ui.add(
+                    egui::TextEdit::singleline(value)
+                        .desired_width(f32::INFINITY)
+                        .hint_text(hint),
+                );
+            };
+
+            ui.vertical_centered(|ui| {
+                show_input_field(ui, &mut self.email, "Email");
+                show_input_field(ui, &mut self.password, "Password");
+                ui.horizontal(|ui| {
+                    if let Some(error) = error {
+                        ui.add(
+                            egui::Label::new(RichText::new(error).color(Color32::RED)).wrap(true),
                         );
                     }
-                    if ui.button("Cancel").clicked() {
-                        self.closed = true;
-                    }
+                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
+                        // RTL
+                        if ui
+                            .add_enabled(error.is_none(), egui::Button::new("Login"))
+                            .clicked()
+                        {
+                            self.signals.push(
+                                StateSignal::Login(self.email.clone(), self.password.clone()).into(),
+                            );
+                        }
+                        if ui.button("Cancel").clicked() {
+                            self.closed = true;
+                        }
+                    });
                 });
             })
             .response
