d2cbda7fc1972c2b871e8c9c224f3138e9b2ee8a
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-03-05 23:33:47 +0300

    Basic context menu for event card

--- src/ui/event_card.rs
+++ src/ui/event_card.rs
@@ -78,14 +78,17 @@ impl<'a> Widget for EventCard<'a> {
                     ui.with_layout(Layout::top_down(Align::LEFT), |ui| {
                         ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
                             if !is_phantom {
-                                ui.spacing_mut().item_spacing = Vec2::new(4., 0.);
-                                if ui.small_button("X").clicked() {
-                                    self.signals
-                                        .push(StateSignal::DeleteEvent(*event_id).into());
-                                }
-                                if ui.small_button("E").clicked() {
-                                    self.signals.push(AppSignal::ChangeEvent(*event_id));
-                                }
+                                ui.menu_button("C", |ui| {
+                                    if ui.button("Edit").clicked() {
+                                        self.signals.push(AppSignal::ChangeEvent(*event_id));
+                                        ui.close_menu();
+                                    }
+                                    if ui.button("Delete").clicked() {
+                                        self.signals
+                                            .push(StateSignal::DeleteEvent(*event_id).into());
+                                        ui.close_menu();
+                                    }
+                                });
                             }
                             ui.with_layout(Layout::left_to_right(Align::TOP), |ui| {
                                 ui.add(egui::Label::new(name).wrap(true));
